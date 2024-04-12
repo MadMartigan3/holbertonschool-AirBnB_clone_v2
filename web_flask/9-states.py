235e2bebd7ee76596b0f2d0b6f8dd888f5eb2a16
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-04-12 12:37:43 +0200

    projet Flask Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,40 +4,31 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from operator import attrgetter
+from models.state import State
 
 if __name__ == "__main__":
     app = Flask(__name__)
 
-    @app.route('/states', strict_slashes=False)
-    def states_id(id):
-        """Display a HTML page with states id"""
-        for state in storage.all("State").values():
-            if state.id == id:
-                return render_template('9-states.html', states=state)
-            return render_template('9-states.html', states=None, mode="none")
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
 
-    @app.route('/cities_by_states', strict_slashes=False)
-    def cities_by_states():
-        """Display a HTML page with a list of all cities by states."""
-        states = storage.all("State")
-        list = states.values()
-        states_list = sorted(list, key=attrgetter('name'))
-        return render_template('8-cities_by_states.html',
-                               states_list=states_list)
 
-    @app.route('/states_list', strict_slashes=False)
-    def states_list():
-        """Display a HTML page with a list of all states."""
-        states = storage.all("State")
-        list = states.values()
-        states_list = sorted(list, key=attrgetter('name'))
-        return render_template('7-states_list.html', states_list=states_list)
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all State objects."""
+    states = storage.all(State)
+    return render_template("9-states.html", states=states, mode="all")
 
-    @app.teardown_appcontext
-    def teardown_db(self):
-        """Remove SqlAlchemy Session."""
-        storage.close()
 
-    if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5000)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with a list of all State id instances."""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", states=state, mode="city")
+    return render_template("9-states.html", states=state, mode="none")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
