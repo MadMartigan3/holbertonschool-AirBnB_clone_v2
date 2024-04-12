d1d7c4ae772572db075f459167ee60480612134e
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-04-12 12:58:09 +0200

    task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,12 +10,6 @@ if __name__ == "__main__":
     app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """Remove the current SQLAlchemy Session."""
-    storage.close()
-
-
 @app.route("/states", strict_slashes=False)
 def states():
     """Displays an HTML page with a list of all State objects."""
@@ -30,6 +24,12 @@ def states_id(id):
         if state.id == id:
             return render_template("9-states.html", states=state, mode="city")
     return render_template("9-states.html", states=state, mode="none")
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
 
 
 if __name__ == "__main__":
